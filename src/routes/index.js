0ecc296ce69f348d6d1717721823d02fcfe908af
jopara <jopara@alumnatflorida.es>
2025-12-22 22:46:29 +0100

    Commit AEV4 2

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,28 @@ const router = Router();
 
 //Rutas GET
 router.get("/rutaGet1", (req, res) => {
-    res.send("hola soy la 1º ruta get");
+    res.send("hola soy el usuario GranMonoBaby");
     console.log("hola en la consola de node, desde la 1º ruta GET en el servidor");
 });
 
 router.get("/rutaGet2", (req, res) => {
-    res.send("hola soy la 2º ruta get");
+    res.send("hola soy el usuario Taez");
     console.log("hola en la consola de node, desde la 2º ruta GET en el servidor");
 });
 
+router.get("/rutaGet3", (req, res) => {
+    res.send("hola soy el usuario Antoga");
+    console.log("hola en la consola de node, desde la 3º ruta GET en el servidor");
+});
 
+router.get("/rutaGet4", (req, res) => {
+    res.send("hola soy el usuario Adalga");
+    console.log("hola en la consola de node, desde la 4º ruta GET en el servidor");
+});
+
+router.get("/rutaGet5", (req, res) => {
+    res.send("hola soy el usuario Gabu");
+    console.log("hola en la consola de node, desde la 5º ruta GET en el servidor");
+});
 
 module.exports = router;
